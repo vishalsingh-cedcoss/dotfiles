d94046f57e03f8702c8268bd78b552cfaf82571a
Vishal Singh_1235 <75413506+vishalsingh-cedcoss@users.noreply.github.com>
2023-06-16 13:14:32 +0530

    Add files via upload

---
+++ nvim/lua/custom/plugins/autopairs.lua
@@ -0,0 +1,13 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+	"windwp/nvim-autopairs",
+	config = function ()
+		require("nvim-autopairs").setup({
+			disable_filetypes = {"TelescopePrompt", "vim"}
+		})
+	end
+}
+
